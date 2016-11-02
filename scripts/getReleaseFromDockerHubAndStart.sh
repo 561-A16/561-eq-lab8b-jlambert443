92133a637381f1e3e16f2438f6ce171af7366a23
Joel Lambert <jlambert443@gmail.com>
2016-11-02 13:56:21 -0400

    lab8 commit3

--- scripts/getReleaseFromDockerHubAndStart.sh
+++ scripts/getReleaseFromDockerHubAndStart.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 set –v docker pull webapp:release
-docker stop webapp:release
-docker rm webapp:release
+docker stop joe443/webapp
+docker rm joe443/webapp
 docker run -itd -p 6539:5000 webapp:release
--- scripts/travisCI.sh
+++ scripts/travisCI.sh
@@ -6,6 +6,6 @@ docker tag webapp:release joe443/webapp:latest
 docker login -u=$USERNAME -p=$PASSWORD
 docker push joe443/webapp:latest
 
-scp -o StrictHostKeyChecking=no ./scripts/getReleaseFromDockerHubAndStart.sh joe443@159.203.33.172:~/
-ssh joe443@159.203.33.172 "chmod +x getReleaseFromDockerHubAndStart.sh"
-ssh joe443@159.203.33.172 "sh getReleaseFromDockerHubAndStart.sh"
+scp -o StrictHostKeyChecking=no ./scripts/getReleaseFromDockerHubAndStart.sh u1064072@159.203.33.172:~/
+ssh u1064072@159.203.33.172 "chmod +x getReleaseFromDockerHubAndStart.sh"
+ssh u1064072@159.203.33.172 "sh getReleaseFromDockerHubAndStart.sh"
